4734bd62d93a63239df87665d7f271ab68b13418
Beau <77253127+ieBeau@users.noreply.github.com>
2024-11-05 16:39:06 -0500

    Add SVG icons and enhance evaluation form functionality and layout

--- Assignment04/scripts.js
+++ Assignment04/scripts.js
@@ -1,3 +1,29 @@
 /* scripts.js */
 
 document.getElementById('course-year').value = new Date().getFullYear();
+
+document.getElementById('evaluation-form').addEventListener('input', function() {
+    const form = this;
+    const submitButton = form.querySelector('.submit');
+});
+
+const fireBtn = document.querySelector(".submit");
+
+fireBtn.addEventListener("click", (event) => {
+    const form = document.getElementById('evaluation-form');
+    if (form.checkValidity()) {
+        event.preventDefault(); 
+        confetti({
+            particleCount: 300,
+            spread: 90,
+            origin: { x: 1, y: 0.9 },
+        });
+
+        confetti({
+            particleCount: 300,
+            spread: 90,
+            origin: { x: 0, y: 0.9 },
+        });
+    }
+});
+
